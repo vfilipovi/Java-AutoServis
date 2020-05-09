789a5d332a62eb6263b6abda766bac7e2cd55078
Filip Vučemilović-Grgić <fvucemilo@tvz.hr>
2020-05-09 18:28:40 +0200

    First commit

---
+++ src/main/java/hr/tvz/autoservis/autoservis/AutoservisApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.autoservis.autoservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoservisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoservisApplication.class, args);
+    }
+
+}
