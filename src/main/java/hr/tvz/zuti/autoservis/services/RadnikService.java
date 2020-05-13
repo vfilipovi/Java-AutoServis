4077eeb35b5dc54dec908de39ff5d6c49a82d502
mzerec <mzerec@tvz.hr>
2020-05-13 22:38:19 +0200

    Implementacija repository-ja, servisa i REST controllera.

--- src/main/java/hr/tvz/zuti/autoservis/services/RadnikService.java
+++ src/main/java/hr/tvz/zuti/autoservis/services/RadnikService.java
@@ -1,9 +1,13 @@
 package hr.tvz.zuti.autoservis.services;
 
+import hr.tvz.zuti.autoservis.domain.Klijent;
 import hr.tvz.zuti.autoservis.domain.Radnik;
+import hr.tvz.zuti.autoservis.repositories.KlijentRepository;
 import hr.tvz.zuti.autoservis.repositories.RadnikRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RadnikService {
@@ -11,8 +15,11 @@ public class RadnikService {
     @Autowired
     private RadnikRepository radnikRepository;
 
-    public Radnik saveOrUpdateRadnik(Radnik radnik) {
+    public Radnik saveOrUpdateRadnik(Radnik radnik) { return radnikRepository.save(radnik); }
 
-        return radnikRepository.save(radnik);
-    }
+    public Optional<Radnik> findRadnikById(Integer radnikId) { return radnikRepository.findById(radnikId); }
+
+    public Iterable<Radnik> findAllRadnici() { return radnikRepository.findAll(); }
+
+    public void deleteRadnikById(Integer radnikId) { radnikRepository.deleteById(radnikId); }
 }
