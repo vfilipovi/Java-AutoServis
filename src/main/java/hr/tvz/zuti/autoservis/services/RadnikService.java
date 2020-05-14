acd2a07d3a163897884e09d3580a10c3d9f21d96
Filip Vučemilović-Grgić <fvucemilo@tvz.hr>
2020-05-14 18:16:42 +0200

    Radni layers added.

--- src/main/java/hr/tvz/zuti/autoservis/services/RadnikService.java
+++ src/main/java/hr/tvz/zuti/autoservis/services/RadnikService.java
@@ -1,9 +1,13 @@
 package hr.tvz.zuti.autoservis.services;
 
 import hr.tvz.zuti.autoservis.domain.Radnik;
+import hr.tvz.zuti.autoservis.domain.Radnik;
+import hr.tvz.zuti.autoservis.exceptions.CustomException;
 import hr.tvz.zuti.autoservis.repositories.RadnikRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RadnikService {
@@ -12,7 +16,21 @@ public class RadnikService {
     private RadnikRepository radnikRepository;
 
     public Radnik saveOrUpdateRadnik(Radnik radnik) {
-
-        return radnikRepository.save(radnik);
+        try {
+            return radnikRepository.save(radnik);
+        } catch (Exception e) {
+            throw new CustomException("Radnik OIB '" + radnik.getOib() + "' već postoji u sustavu.");
+        }
     }
+
+    public Optional<Radnik> findRadnikById(Integer radnikId) {
+        Optional<Radnik> radnik = radnikRepository.findById(radnikId);
+        if (radnik.isPresent())
+            return radnik;
+        else throw new CustomException("Ne postoji radnik s ID-em '" + radnikId + "'.");
+    }
+
+    public Iterable<Radnik> findAllRadniki() { return radnikRepository.findAll(); }
+
+    public void deleteRadnikById(Integer radnikId) { radnikRepository.deleteById(radnikId); }
 }
--- src/main/java/hr/tvz/zuti/autoservis/web/RadnikController.java
+++ src/main/java/hr/tvz/zuti/autoservis/web/RadnikController.java
@@ -1,19 +1,17 @@
 package hr.tvz.zuti.autoservis.web;
 
 import hr.tvz.zuti.autoservis.domain.Radnik;
-import hr.tvz.zuti.autoservis.services.MapValidationErrorService;
 import hr.tvz.zuti.autoservis.services.RadnikService;
+import hr.tvz.zuti.autoservis.services.MapValidationErrorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/radnici")
@@ -25,7 +23,7 @@ public class RadnikController {
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
 
-
+    @PostMapping("")
     public ResponseEntity<?> createNewRadnik(@Valid @RequestBody Radnik _radnik, BindingResult result) {
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
@@ -35,5 +33,45 @@ public class RadnikController {
         Radnik radnik = radnikService.saveOrUpdateRadnik(_radnik);
         //  ResponseEntity<> is short for ResponseEntity<Radnik>
         return new ResponseEntity<>(radnik, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{radnikId}")
+    public ResponseEntity<Optional<Radnik>> getRadnikById(@PathVariable Integer radnikId) {
+
+        Optional<Radnik> radnik = radnikService.findRadnikById(radnikId);
+        return new ResponseEntity<>(radnik, HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public Iterable<Radnik> getAllRadniki() { return radnikService.findAllRadniki(); }
+
+    @PutMapping("/{radnikId}")
+    public ResponseEntity<?> updateRadnikById(@Valid @RequestBody Radnik _radnik, @PathVariable Integer radnikId, BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+
+        if (errorMap != null) return errorMap;
+
+        Optional<Object> updatedRadnik = radnikService.findRadnikById(radnikId).map(radnik -> {
+            radnik.setIme(_radnik.getIme());
+            radnik.setPrezime(_radnik.getPrezime());
+            radnik.setOib(_radnik.getOib());
+            radnik.setIznosOsnovice(_radnik.getIznosOsnovice());
+            radnik.setKoefPlace(_radnik.getKoefPlace());
+            radnik.setStatusRadnogOdnosa(_radnik.getStatusRadnogOdnosa());
+            radnik.setVrstaRadnogOdnosa(_radnik.getVrstaRadnogOdnosa());
+
+            return radnikService.saveOrUpdateRadnik(radnik);
+        });
+
+        return new ResponseEntity<>(updatedRadnik, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{radnikId}")
+    public  ResponseEntity<?> deleteRadnik(@PathVariable Integer radnikId) {
+
+        radnikService.deleteRadnikById(radnikId);
+
+        return  new ResponseEntity<>("Korisnik s ID-em " + radnikId + " je izbrisan.", HttpStatus.NO_CONTENT);
     }
 }
